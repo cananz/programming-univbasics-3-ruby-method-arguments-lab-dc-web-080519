77753f63d0fdac20e8a68e189b3858adaee445ff
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-05 15:29:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,6 @@ def introduction_with_language(name,language)
 end
 
 #Define a Method With an Optional Argument
-def introduction_with_language_optional(name,language)
+def introduction_with_language_optional(name,language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
