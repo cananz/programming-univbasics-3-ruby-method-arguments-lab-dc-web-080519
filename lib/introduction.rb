a49dacff93d0b3cdbb43e6e395025c37f1db390f
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-05 15:21:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,11 @@
 #Define a Method That Takes in and Uses an Argument
 
 def introduction(name)
-puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 #Define a Method That Takes in and Uses Two Arguments
 
 def introduction_with_language(name,language)
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
